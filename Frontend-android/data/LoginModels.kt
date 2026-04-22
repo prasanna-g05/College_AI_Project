341c8e3b63a59315677b3e6e17660b0af98cebf1
Prasanna Nagsen Gajbhiye <prasannag2631@gmail.com>
2026-04-22 18:53:06 +0530

    Update LoginModels.kt

--- Frontend-android/data/LoginModels.kt
+++ Frontend-android/data/LoginModels.kt
@@ -5,34 +5,45 @@ import com.google.gson.annotations.SerializedName
 data class LoginRequest(
     @SerializedName("erp_number")
     val erpNumber: String,
+
     @SerializedName("password")
     val password: String
 )
 
-
 data class LoginResponse(
     @SerializedName("access_token")
-    val accessToken: String,
+    val accessToken: String = "",
+
     @SerializedName("token_type")
-    val tokenType: String,
+    val tokenType: String = "",
+
     @SerializedName("student")
     val student: Student? = null
 )
 
+data class AuthSession(
+    val accessToken: String,
+    val tokenType: String,
+    val student: Student
+)
 
 data class RegisterRequest(
     @SerializedName("erp_number")
     val erpNumber: String,
+
     val name: String,
     val year: String,
     val semester: String,
-    val rollNumber:String,
+
+    @SerializedName("roll_number")
+    val rollNumber: String,
+
     val password: String,
     val branch: String = "Information Technology",
     val section: String
 )
 
 data class RegisterResponse(
-    val message: String,
+    val message: String = "",
     val user: Student? = null
 )
